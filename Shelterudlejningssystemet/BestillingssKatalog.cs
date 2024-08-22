9b9e2b2b86fce898cbb07b901a5d756eb9343559
saram <saram@DESKTOP-V6UK9BQ>
2024-08-22 12:02:06 +0200

    Add project files.

---
+++ Shelterudlejningssystemet/BestillingssKatalog.cs
@@ -0,0 +1,42 @@
+﻿using Shelterudlejningssystemet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shelterudlejningssystemet
+{
+     public class BestillingsKatalog
+    {
+        private List<FdfKreds> Fdfkreds; 
+        
+        //constructor der sætter listen igang 
+        public BestillingsKatalog()
+        { 
+            Fdfkreds = new List<FdfKreds>();
+        }
+
+        //tilføje fdfkreds til listen
+        public void TilføjFdfKreds(FdfKreds fdfKreds)
+        {
+            fdfKreds.Add(fdfKreds: fdfKreds);
+        }
+        public List <FdfKreds> GetFdfKreds()
+        {
+            return Fdfkreds;
+        }
+        //printkataloget
+        public void PrintKataloget()
+        {
+            Console.WriteLine(Fdfkreds);
+        }
+
+    }
+
+}
+
+
+
+
+
---
+++ Shelterudlejningssystemet/FdfKreds.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shelterudlejningssystemet
+{
+    public class FdfKreds
+    {
+        public string Name {get; set; }
+        public int Aldersgruppe { get; set; }
+        public int AntalDeltagere { get; set; }
+        public int Id { get; set; }
+        public int BookingDato { get; set; }
+        public int BookingTidspunkt {get; set; }
+
+        //constructor
+        public FdfKreds(string name, int aldersgruppe, int antaldeltagere,int id, int bookingdato, int bookingtidspunkt)
+        {
+            Name = name;
+            Aldersgruppe = aldersgruppe;
+            AntalDeltagere = antaldeltagere;
+            Id= id;
+            BookingDato = bookingdato;
+            BookingTidspunkt = bookingtidspunkt;
+        }
+
+        internal void Add(FdfKreds fdfKreds)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
